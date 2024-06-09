5dfaff76ff1adf1984890f610fa639696ae526da
Thiago Teodoro <thiago.teodoro.santana@eaportal.org>
2024-06-09 18:43:26 +0000

    feat: instalação e implantação do reac-router-dom e do styled-components

---
+++ src/styles/theme.js
@@ -0,0 +1,8 @@
+const theme = {
+    homeBackground: "#131518",
+    cardGradient: "radial-gradient(98.96% 98.96% at 50% 0%, #232A34 0%, #181E27 100%)",
+    submitButtonColor: "#969FAD",
+    white: "#FFF",
+    feedbackButtonColor: "#262E38",
+    feedbackButtonText: "#7C8798"
+}
