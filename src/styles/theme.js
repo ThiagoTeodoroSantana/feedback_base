68c1e0acf648ff5034d1f26a61dc9e5402515a61
Thiago Teodoro <thiago.teodoro.santana@eaportal.org>
2024-06-09 19:23:14 +0000

    style: estilização do h1 e do p do card

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -1,6 +1,7 @@
 export const theme = {
     homeBackground: "#131518",
     cardGradient: "radial-gradient(98.96% 98.96% at 50% 0%, #232A34 0%, #181E27 100%)",
+    textColor: "#969FAD",
     submitButtonColor: "#969FAD",
     white: "#FFF",
     feedbackButtonColor: "#262E38",
